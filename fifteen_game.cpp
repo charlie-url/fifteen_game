9a3a399bdaf026c17d2aa1e937a67777c7ff65cc
fivetwofoureight <charliekw@gmail.com>
2017-11-08 16:04:08 -0600

    added instructions button and info

--- fifteen_game.cpp
+++ fifteen_game.cpp
@@ -8,10 +8,11 @@ struct Splash_screen : Graph_lib::Window {
 	Splash_screen(Point xy, int w, int h, const string& title)
 		:Window{ xy,w,h,title },
 		play_button{ Point{ 360-64,360-32 }, 128, 64, "Play",  [](Address, Address pw) { reference_to<Splash_screen>(pw).play(); } },
+		show_instructions{ Point{ 360 - 64,360 + 32 }, 128, 64, "Instuctions",  [](Address, Address pw) { reference_to<Splash_screen>(pw).instruct(); } },
 		button_pushed{ false }
 	{
 		attach(play_button);
-
+		attach(show_instructions);
 	}
 	void wait_for_button() {
 		while (!button_pushed) {
@@ -22,12 +23,20 @@ struct Splash_screen : Graph_lib::Window {
 	}
 
 private:
-
+	string game_name = "Fifteen Game";
+	string team_info = "Team 41: TeamName";
+	string team_roster = "Charles Wong/nSavannah Yu/nCindy Zhang/nEric Zhang";
+	string instructions = "Instructions:/n/t1. Do/n/t2.Something";
+	Button show_instructions;
 	Button play_button;
 	bool button_pushed;
 
 	void play() {
 		cout << "[next screen]" << endl;
+		Fl::redraw();
+	}
+	void instruct() {
+		cout << "[instructions]" << endl;
 		Fl::redraw();
 	}
 };
