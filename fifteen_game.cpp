80310b92ef251df617ac962a93a30fdcff5286f7
Charlie <charliekw@gmail.com>
2017-11-08 16:32:26 -0600

    prepare task_2 for work (#2)
    * created fifteen_game.h
    
    header file for including other .cpp
    
    * created Game_window
    
    * Copied Game_window framework from Simple_window
    
    * Removed Game_window to test compile
    
    added rest of required libraries
    
    * created Splash_screen
    
    window with a button centered
    
    * added instructions button and info
    
    * changed info to Text and attached
    
    also fixed constructor order to remove warnings

--- fifteen_game.cpp
+++ fifteen_game.cpp
@@ -2,12 +2,54 @@
 #include "Window.h"
 #include "Graph.h"
 #include "FL/Fl_JPEG_Image.H"
+#include "Simple_window.h"
+
+struct Splash_screen : Graph_lib::Window {
+	Splash_screen(Point xy, int w, int h, const string& title)
+		:Window{ xy,w,h,title },
+		show_instructions{ Point{ 360 - 64,360 + 32 }, 128, 64, "Instuctions",  [](Address, Address pw) { reference_to<Splash_screen>(pw).instruct(); } },
+		play_button{ Point{ 360 - 64,360 - 32 }, 128, 64, "Play",  [](Address, Address pw) { reference_to<Splash_screen>(pw).play(); } },
+		button_pushed{ false }
+	{
+		attach(play_button);
+		attach(show_instructions);
+		attach(game_name);
+		attach(team_info);
+		attach(team_roster);
+		attach(instructions);
+	}
+	void wait_for_button() {
+		while (!button_pushed) {
+			Fl::wait();
+		}
+		button_pushed = false;
+		play();
+	}
+
+private:
+	Text game_name = Text{ Point{100,100}, "Fifteen Game" };
+	Text team_info = Text{ Point{ 100,150 }, "Team 41: TeamName" };
+	Text team_roster = Text{ Point{ 100,200 }, "Charles Wong Savannah Yu Cindy Zhang Eric Zhang" };
+	Text instructions = Text{ Point{ 100,250 }, "Instructions:1. Do 2.Something" };
+	Button show_instructions;// = Button{ Point{ 360 - 64,360 + 32 }, 128, 64, "Instuctions",  [](Address, Address pw) { reference_to<Splash_screen>(pw).instruct(); } };
+	Button play_button;
+	bool button_pushed;
+
+	void play() {
+		cout << "[next screen]" << endl;
+		Fl::redraw();
+	}
+	void instruct() {
+		cout << "[instructions]" << endl;
+		Fl::redraw();
+	}
+};
 
 
 int main() {
 	try {
-		
-
+		Splash_screen splash(Point(0, 0), 720, 720, "Splash Screen");
+		splash.wait_for_button();
 
 
 
