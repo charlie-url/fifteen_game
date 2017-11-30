649a903261663fc21449946d78586e65a0e602c1
fivetwofoureight <charliekw@gmail.com>
2017-11-30 16:24:24 -0600

    graphical fixes to level select

--- Level_select.cpp
+++ Level_select.cpp
@@ -3,10 +3,11 @@
 
 	Level_select::Level_select(Point xy, int w, int h, const string& title, string user)
 		:Project_window{ xy,w,h,title },
-		ten_button{ Point(200,50), 320, 100, "10", [](Address, Address pw) { reference_to<Level_select>(pw).set_state(Game_state(Game_10));} },
-		twenty_button{ Point(200,150), 320, 100, "20", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_20)); } },
-		forty_button{ Point(200,250), 320, 100, "40", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_40)); } },
-		eighty_button{ Point(200,350), 320, 100, "80", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_80)); } }
+		ten_button{ Point(200,90+128), 320, 90, "Easy", [](Address, Address pw) { reference_to<Level_select>(pw).set_state(Game_state(Game_10));} },
+		twenty_button{ Point(200,90+228), 320, 90, "Normal", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_20)); } },
+		forty_button{ Point(200,90+328), 320, 90, "Advanced", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_40)); } },
+		eighty_button{ Point(200,90+428), 320, 90, "Expert", [](Address, Address pw) {reference_to<Level_select>(pw).set_state(Game_state(Game_80)); } },
+		choose_text{ Text{ Point{ 360 - (20 * 15) / 2,90 + 64 },"Choose a difficulty:" } }
 	{
 		attach(username);
 		set_username(user);
@@ -15,5 +16,7 @@
 		attach(forty_button);
 		attach(eighty_button);
 		attach(username);
+		choose_text.set_font_size(32);
+		attach(choose_text);
 	}
 
