059bd7768a030d1a7c0bf468598e0be67441b241
fivetwofoureight <charliekw@gmail.com>
2017-11-30 16:35:41 -0600

    graphical fixes on instruction screen

--- Instruct_screen.cpp
+++ Instruct_screen.cpp
@@ -4,10 +4,12 @@
 
 	Instruct_screen::Instruct_screen(Point xy, int w, int h, const string& title)
 		:Project_window{ xy,w,h,title },
-		go_to_levels{ Point{ 360 - 64,360 - 32 }, 128, 64, "Start",  [](Address, Address pw) { reference_to<Instruct_screen>(pw).set_state(Game_state(Level)); } }
+		go_to_levels{ Point{ 360 - 64,360 + 64 }, 128, 64, "Play",  [](Address, Address pw) { reference_to<Instruct_screen>(pw).set_state(Game_state(Level)); } }
 	{
 		attach(instruct1);
 		attach(instruct2);
 		attach(go_to_levels);
+		how_to_play.set_font_size(64);
+		attach(how_to_play);
 	}
 
